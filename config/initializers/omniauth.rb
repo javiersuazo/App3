7d952f42ee92d512640dbf0ce96ba148a232f288
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-12-26 01:14:47 -0600

    asdkl

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, "68010f549d2f6d19a90172a1f669f4d5", "f048d44b283a40d0ea44ea4404337cbd"
+	provider :facebook, "214871128595934", "83ed75d122155ee3abfd874678b4efc2"
 end
